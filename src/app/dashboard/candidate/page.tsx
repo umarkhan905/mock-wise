55339c59455ba16ed32712299c36659959a0d5ab
umarkhan905 <itxumarkhan905@gmail.com>
2025-05-09 08:13:15 +0500

    Add new pages for candidate dashboard: chatbot, job interviews, messages, and mock interviews

--- src/app/dashboard/candidate/page.tsx
+++ src/app/dashboard/candidate/page.tsx
@@ -1,5 +1,12 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 }
