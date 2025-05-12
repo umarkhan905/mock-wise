dc398aa35e6112e019cf85bcb4a7091faa61a66f
umarkhan905 <itxumarkhan905@gmail.com>
2025-05-12 10:31:41 +0500

    Add CreateInterview component for selecting interview type

---
+++ src/app/dashboard/recruiter/create-interview/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+export default function CreateInterview() {
+  const [active, setActive] = useState<"mcq" | "voice" | null>(null);
+  const router = useRouter();
+
+  const handleClickNext = () => {
+    if (!active) return;
+    router.push(`/dashboard/recruiter/create-interview/${active}`);
+  };
+
+  return (
+    <div className="w-full flex items-center justify-center min-h-[calc(100vh-104px)]">
+      <Card className="max-w-lg w-full">
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Create Interview</CardTitle>
+          <CardDescription className="">
+            Select the type of interview you want to create
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent className="grid grid-cols-2 gap-4">
+          {/* MCQ */}
+          <div
+            className={`border p-2 rounded-md flex items-center justify-center min-h-40  cursor-pointer ${active === "mcq" ? "bg-primary/20 text-primary" : "bg-primary/10"}`}
+            onClick={() => setActive((prev) => (prev === "mcq" ? null : "mcq"))}
+          >
+            <span className="font-semibold">MCQ Interview</span>
+          </div>
+
+          {/* Voice */}
+          <div
+            className={`border p-2 rounded-md flex items-center justify-center min-h-40  cursor-pointer ${active === "voice" ? "bg-primary/20 text-primary" : "bg-primary/10"}`}
+            onClick={() =>
+              setActive((prev) => (prev === "voice" ? null : "voice"))
+            }
+          >
+            <span className="font-semibold">Voice Interview</span>
+          </div>
+        </CardContent>
+
+        <CardFooter>
+          <Button
+            className="w-full min-h-10 text-white"
+            disabled={!active}
+            onClick={handleClickNext}
+          >
+            Next
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
