8536134e179286bbce9244209ece92faa2e1fe1e
umarkhan905 <itxumarkhan905@gmail.com>
2025-05-22 07:55:48 +0500

    Wrap children in PresenceHandlerProvider within Layout component to manage user presence

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,11 @@
 import { AuthContextProvider } from "@/context/AuthStore";
 import React from "react";
+import { PresenceHandlerProvider } from "./_components/providers/PresenceHandlerProvider";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
+  return (
+    <AuthContextProvider>
+      <PresenceHandlerProvider>{children}</PresenceHandlerProvider>
+    </AuthContextProvider>
+  );
 }
