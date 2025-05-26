8229ef42cc6c879ff3a922385ce77c172c97c8ae
umarkhan905 <itxumarkhan905@gmail.com>
2025-05-26 18:03:03 +0500

    Add Account page for candidate and recruiter dashboards

---
+++ src/app/dashboard/candidate/account/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Account() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">Manage Account</h1>
+    </section>
+  );
+}
---
+++ src/app/dashboard/recruiter/account/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Account() {
+  return (
+    <section className="space-y-6">
+      <h1 className="text-3xl font-bold">Manage Account & Settings</h1>
+    </section>
+  );
+}
