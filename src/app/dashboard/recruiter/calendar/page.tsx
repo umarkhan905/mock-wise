48532ee357ba9eb37be3323ecbe44ec2f79a4ae6
umarkhan905 <itxumarkhan905@gmail.com>
2025-05-26 17:48:55 +0500

    Implement Calendar component with event scheduling and popover functionality

---
+++ src/app/dashboard/recruiter/calendar/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useAuthContext } from "@/context/AuthStore";
+import { useQuery } from "convex/react";
+import React, { useEffect, useState } from "react";
+import { api } from "../../../../../convex/_generated/api";
+import EventsCalendar from "@/components/calendar/EventsCalendar";
+import { EventImpl } from "@fullcalendar/core/internal";
+import EventPopOver from "@/components/calendar/EventPopOver";
+
+interface Event {
+  id: string;
+  title: string;
+  start: Date;
+  end?: Date;
+  allDay?: boolean;
+  description?: string;
+}
+
+export default function Calendar() {
+  const [events, setEvents] = useState<Event[]>([]);
+  const [openPopOver, setOpenPopOver] = useState<boolean>(false);
+  const [selectedEvent, setSelectedEvent] = useState<EventImpl | null>(null);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const { user } = useAuthContext();
+  const interviews = useQuery(
+    api.recruiter.getScheduledInterviews,
+    user ? { userId: user._id } : "skip"
+  );
+
+  const onClosePopOver = () => {
+    setOpenPopOver(false);
+    setSelectedEvent(null);
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    if (interviews) {
+      const formattedEvents = interviews.map((interview) => ({
+        id: interview._id,
+        title: interview.title,
+        start: new Date(interview.scheduledAt!),
+        description: interview.description || "No description",
+      }));
+      setEvents(formattedEvents);
+    }
+  }, [interviews]);
+
+  return (
+    <>
+      <EventsCalendar
+        events={events}
+        setOpenPopOver={setOpenPopOver}
+        setSelectedEvent={setSelectedEvent}
+        setAnchorEl={setAnchorEl}
+      />
+
+      {selectedEvent && anchorEl && (
+        <EventPopOver
+          event={selectedEvent}
+          open={openPopOver}
+          onOpenChange={onClosePopOver}
+          anchorEl={anchorEl} // Pass the anchor element to the popover
+          userRole={user?.role || "candidate"} // Assuming user role is available
+        />
+      )}
+    </>
+  );
+}
