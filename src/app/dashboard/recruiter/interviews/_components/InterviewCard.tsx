c1ed35d9ae6169564465bcd25abe720068d8b709
umarkhan905 <itxumarkhan905@gmail.com>
2025-05-26 07:37:41 +0500

    feat: add InterviewCard and InterviewFilters components to enhance interview management

---
+++ src/app/dashboard/recruiter/interviews/_components/InterviewCard.tsx
@@ -0,0 +1,127 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useCopy } from "@/hooks/useCopy";
+import { Interview } from "@/types";
+import { Check, Copy } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+interface User {
+  companyName: string | undefined;
+  companyLogo: string | undefined;
+}
+
+interface Props {
+  interview: Interview;
+  user: User;
+}
+
+type Status = "pending" | "created" | "scheduled" | "expired";
+
+const getInterviewStatus = (status: Status) => {
+  switch (status) {
+    case "pending":
+      return "Pending";
+    case "created":
+      return "Active";
+    case "scheduled":
+      return "Scheduled";
+    case "expired":
+      return "Expired";
+  }
+};
+
+const getStatusBasedClasses = (status: Status) => {
+  switch (status) {
+    case "pending":
+      return "bg-yellow-500/10 text-yellow-500";
+    case "created":
+      return "bg-primary/10 text-primary";
+    case "scheduled":
+      return "bg-blue-500/10 text-blue-500";
+    case "expired":
+      return "bg-destructive/10 text-destructive";
+  }
+};
+
+export default function InterviewCard({ interview, user }: Props) {
+  const { isCopied, copyToClipboard } = useCopy();
+
+  if (!interview) return null;
+
+  return (
+    <Card className="gap-2">
+      <CardHeader className="px-3">
+        <div className="flex items-center gap-3">
+          <Avatar className="size-12">
+            <AvatarImage src={user.companyLogo} />
+            <AvatarFallback className="bg-primary/10 text-primary text-xl">
+              {user.companyName?.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+
+          <div>
+            <CardTitle className="line-clamp-1">{interview.title}</CardTitle>
+
+            <div className="flex items-center gap-2 mt-1">
+              <CardDescription className="line-clamp-1">
+                {interview.role}
+              </CardDescription>
+              <Badge
+                className={`${getStatusBasedClasses(interview.status)} rounded-full`}
+                variant={"outline"}
+              >
+                {getInterviewStatus(interview.status)}
+              </Badge>
+            </div>
+          </div>
+        </div>
+      </CardHeader>
+
+      <CardContent className="px-3">
+        <p className="line-clamp-3 text-muted-foreground text-sm">
+          {interview.description}
+        </p>
+
+        <Button className="min-h-10 w-full mt-2 text-white" asChild>
+          <Link href={`/interview/${interview._id}/recruiter/details`}>
+            View Interview
+          </Link>
+        </Button>
+
+        <Button
+          className={`min-h-10 w-full mt-2 text-white ${interview.status !== "expired" && !isCopied && "hover:bg-primary/10 hover:text-white"} ${isCopied && "bg-green-500/10 text-green-500 hover:hover:bg-green-500/10 hover:text-green-500"}`}
+          variant={interview.status === "expired" ? "destructive" : "outline"}
+          disabled={interview.status === "expired"}
+          onClick={() =>
+            copyToClipboard(
+              `${process.env.NEXT_PUBLIC_APP_BASE_URL}/interview/${interview._id}`
+            )
+          }
+        >
+          {interview.status === "expired" ? (
+            "Expired"
+          ) : isCopied ? (
+            <>
+              <Check className="size-5" />
+              <span>Link Copied</span>
+            </>
+          ) : (
+            <>
+              <Copy className="size-5" />
+              <span>Copy Link</span>
+            </>
+          )}
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
--- src/app/dashboard/recruiter/interviews/page.tsx
+++ src/app/dashboard/recruiter/interviews/page.tsx
@@ -1,5 +1,104 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import React, { useState } from "react";
+import InterviewFilters from "./_components/InterviewFilters";
+import { RecruiterFilters } from "@/types";
+import { useQuery } from "convex/react";
+import { api } from "../../../../../convex/_generated/api";
+import { useAuth } from "@clerk/nextjs";
+import InterviewCard from "./_components/InterviewCard";
+import { defaultRecruiterFilters } from "@/constants";
 
 export default function Interviews() {
-  return <div>Interviews</div>;
+  const [search, setSearch] = useState<string>("");
+  const [filters, setFilters] = useState<RecruiterFilters>(
+    defaultRecruiterFilters
+  );
+
+  // query
+  const { userId, isLoaded } = useAuth();
+
+  const user = useQuery(
+    api.users.getUserByClerkId,
+    userId ? { clerkId: userId } : "skip"
+  );
+
+  const interviews = useQuery(
+    api.recruiter.getInterviews,
+    user
+      ? {
+          userId: user._id,
+          assessment:
+            filters.assessment === "all" ? undefined : filters.assessment,
+          status: filters.status === "all" ? undefined : filters.status,
+          difficulty:
+            filters.difficulty === "all" ? undefined : filters.difficulty,
+          experience:
+            filters.experience === "all" ? undefined : filters.experience,
+          orderBy: filters.oderBy,
+        }
+      : "skip"
+  );
+
+  const filteredInterviews =
+    interviews &&
+    interviews.filter((interview) => {
+      return interview.title.toLowerCase().includes(search.toLowerCase());
+    });
+
+  if (!isLoaded) {
+    // Handle loading state
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className="space-y-6">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">All Interviews</h1>
+        <Button asChild className="text-white min-h-10">
+          <Link href="/dashboard/recruiter/create-interview">
+            <Plus className="size-5 mr-2" />
+            <span>Create Interview</span>
+          </Link>
+        </Button>
+      </div>
+
+      <InterviewFilters
+        filters={filters}
+        setFilters={setFilters}
+        search={search}
+        setSearch={setSearch}
+      />
+
+      <div className="space-y-4">
+        {interviews === undefined && <p>Loading...</p>}
+        {filteredInterviews && filteredInterviews.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {filteredInterviews.map((interview) => (
+              <InterviewCard
+                key={interview._id}
+                interview={interview}
+                user={{
+                  companyLogo: user?.companyLogo,
+                  companyName: user?.companyName,
+                }}
+              />
+            ))}
+          </div>
+        ) : (
+          filteredInterviews && (
+            <div className="min-h-[calc(100vh-235px)] flex items-center justify-center flex-col">
+              <span className="text-4xl mb-2">🤔</span>
+              <p className="text-muted-foreground text-sm">
+                No interviews found. Try adjusting your filters.
+              </p>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
 }
